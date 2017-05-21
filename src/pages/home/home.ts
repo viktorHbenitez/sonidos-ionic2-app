f34cefa33826ff1f2552fb15b63e3a9e2f9146d5
viktor hugo <victorHbenitez85@hotmail.com>
2017-05-21 00:46:29 -0500

    Mostrando un listado de los animales usando ngFor

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { ANIMALES } from "../../data/data.animales";  //importamos el model
+
+// Usamos la estructura de datos de tipo animal
+import { Animal } from '../../interfaces/animal.interface';  //creamos interface para manipular los datos de la Data
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales : Animal[] = [];
+
+  constructor() {
+
+    // slice : crea un clone del objeto
+    this.animales = ANIMALES.slice(0);  // la interfaces de animales debe los mismos atributos de la data ANIMAL
 
   }
 
